370aa21f23558f59948152c5547898b854323caa
FreitasMatheus <132593905+FreitasFMatheus@users.noreply.github.com>
2023-11-18 11:50:56 -0300

    Update biblioteca.h

--- biblioteca.h
+++ biblioteca.h
@@ -7,8 +7,6 @@
 #define MAX_CLIENTES 100
 #define MAX_TRANSACOES 1000
 
-
-
 #define MAX_CPF_SIZE (MAX_CPF + 1)
 #define MAX_NOME_SIZE (MAX_NOME + 1)
 #define MAX_SENHA_SIZE (MAX_SENHA + 1)
@@ -37,23 +35,30 @@ typedef struct {
     float valor;
 } Transacao;
 
+typedef struct {
+    Cliente clientes[MAX_CLIENTES];
+    int numClientes;
+    Transacao transacoes[MAX_TRANSACOES];
+    int numTransacoes;
+} BancoDados;
+
 // Funções relacionadas a clientes
-void adicionarCliente(Cliente c);
-Cliente *buscarClientePorCPF(const char *cpf);
-void removerCliente(const char *cpf);
-void listarClientes();
-void debitar(const char *cpf, const char *senha, float valor);
-void depositar(const char *cpf, float valor);
-void extrato(const char *cpf, const char *senha);
-void transferir(char cpf[], char senha[], char cpfDestino[], float valor);
+void adicionarCliente(BancoDados *dados, Cliente c);
+Cliente *buscarClientePorCPF(BancoDados *dados, const char *cpf);
+void removerCliente(BancoDados *dados, const char *cpf);
+void listarClientes(BancoDados *dados);
+void debitar(BancoDados *dados, const char *cpf, const char *senha, float valor);
+void depositar(BancoDados *dados, const char *cpf, float valor);
+void extrato(BancoDados *dados, const char *cpf, const char *senha);
+void transferir(BancoDados *dados, char cpf[], char senha[], char cpfDestino[], float valor);
 
 // Funções relacionadas a transações
-void registrarTransacao(Transacao t);
+void registrarTransacao(BancoDados *dados, Transacao t);
 
 // Funções para salvar e carregar dados de arquivos
-void salvarClientesArquivo();
-void carregarClientesArquivo();
-void salvarTransacoesArquivo();
-void carregarTransacoesArquivo();
+void salvarClientesArquivo(BancoDados *dados);
+void carregarClientesArquivo(BancoDados *dados);
+void salvarTransacoesArquivo(BancoDados *dados);
+void carregarTransacoesArquivo(BancoDados *dados);
 
 #endif // BIBLIOTECA_H
