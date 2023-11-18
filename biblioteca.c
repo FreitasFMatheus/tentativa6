f6dad72745709eec8a546efa90215f6d03b6ea45
FreitasMatheus <132593905+FreitasFMatheus@users.noreply.github.com>
2023-11-18 11:51:30 -0300

    Update biblioteca.c

--- biblioteca.c
+++ biblioteca.c
@@ -6,73 +6,65 @@
 #include <stdio.h>
 #include <string.h>
 
-//"olá Leo, tudo bem contigo não coloquei nome de filme nesse projeto igual no semestre passado, mas ainda assim espero que tenha ficado menos cansativo de ler durante a correção"
-
 // Definindo constantes para os nomes dos arquivos
 #define CLIENTES_FILE "clientes.bin"
 #define TRANSACOES_FILE "transacoes.bin"
 #define EXTRATO_FILE "extrato.txt"
 
-// Arrays estáticos para armazenar clientes e transações
-static Cliente clientes[MAX_CLIENTES];
-static int numClientes = 0;  // Contador de clientes
-static Transacao transacoes[MAX_TRANSACOES];
-static int numTransacoes = 0;  // Contador de transações
-
 // Função para adicionar um cliente à lista de clientes
-void adicionarCliente(Cliente c) {
-    if (numClientes < MAX_CLIENTES) {
-        clientes[numClientes] = c;
-        numClientes++;
+void adicionarCliente(BancoDados *dados, Cliente c) {
+    if (dados->numClientes < MAX_CLIENTES) {
+        dados->clientes[dados->numClientes] = c;
+        dados->numClientes++;
     } else {
         printf("Limite de clientes atingido.\n");
     }
 }
 
 // Busca um cliente pelo CPF e retorna um ponteiro para o mesmo
-Cliente *buscarClientePorCPF(const char *cpf) {
-    for (int i = 0; i < numClientes; i++) {
-        if (strcmp(clientes[i].cpf, cpf) == 0) {
-            return &clientes[i];
+Cliente *buscarClientePorCPF(BancoDados *dados, const char *cpf) {
+    for (int i = 0; i < dados->numClientes; i++) {
+        if (strcmp(dados->clientes[i].cpf, cpf) == 0) {
+            return &dados->clientes[i];
         }
     }
-    return NULL;  // Retorna NULL se não encontrar o cliente
+    return NULL;
 }
 
 // Remove um cliente pelo CPF
-void removerCliente(const char *cpf) {
+void removerCliente(BancoDados *dados, const char *cpf) {
     int index = -1;
-    for (int i = 0; i < numClientes; i++) {
-        if (strcmp(clientes[i].cpf, cpf) == 0) {
+    for (int i = 0; i < dados->numClientes; i++) {
+        if (strcmp(dados->clientes[i].cpf, cpf) == 0) {
             index = i;
             break;
         }
     }
 
     if (index != -1) {
-        for (int i = index; i < numClientes - 1; i++) {
-            clientes[i] = clientes[i + 1];
+        for (int i = index; i < dados->numClientes - 1; i++) {
+            dados->clientes[i] = dados->clientes[i + 1];
         }
-        numClientes--;
+        dados->numClientes--;
     } else {
         printf("Cliente não encontrado.\n");
     }
 }
 
 // Função que lista todos os clientes registrados
-void listarClientes() {
-    for (int i = 0; i < numClientes; i++) {
+void listarClientes(BancoDados *dados) {
+    for (int i = 0; i < dados->numClientes; i++) {
         printf("Nome: %s, CPF: %s, Tipo de Conta: %s, Saldo: %.2f\n",
-               clientes[i].nome,
-               clientes[i].cpf,
-               clientes[i].tipo == COMUM ? "Comum" : "Plus",
-               clientes[i].saldo);
+               dados->clientes[i].nome,
+               dados->clientes[i].cpf,
+               dados->clientes[i].tipo == COMUM ? "Comum" : "Plus",
+               dados->clientes[i].saldo);
     }
 }
 
 // Função para debitar um valor da conta de um cliente
-void debitar(const char *cpf, const char *senha, float valor) {
-    Cliente *cliente = buscarClientePorCPF(cpf);
+void debitar(BancoDados *dados, const char *cpf, const char *senha, float valor) {
+    Cliente *cliente = buscarClientePorCPF(dados, cpf);
     if (cliente && strcmp(cliente->senha, senha) == 0) {
         float taxa = (cliente->tipo == COMUM) ? 0.05 : 0.03;
         float debito = valor + valor * taxa;
@@ -83,15 +75,15 @@ void debitar(const char *cpf, const char *senha, float valor) {
         strcpy(t.cpf, cpf);
         t.tipo = DEBITO;
         t.valor = debito;
-        registrarTransacao(t);
+        registrarTransacao(dados, t);
     } else {
         printf("CPF ou senha incorretos.\n");
     }
 }
 
 // Função para depositar um valor na conta de um cliente
-void depositar(const char *cpf, float valor) {
-    Cliente *cliente = buscarClientePorCPF(cpf);
+void depositar(BancoDados *dados, const char *cpf, float valor) {
+    Cliente *cliente = buscarClientePorCPF(dados, cpf);
     if (cliente) {
         cliente->saldo += valor;
 
@@ -100,22 +92,21 @@ void depositar(const char *cpf, float valor) {
         strcpy(t.cpf, cpf);
         t.tipo = DEPOSITO;
         t.valor = valor;
-        registrarTransacao(t);
+        registrarTransacao(dados, t);
     } else {
         printf("Cliente não encontrado.\n");
     }
 }
 
-// Função para gerar o extrato das transações de um cliente
-void extrato(const char *cpf, const char *senha) {
-    Cliente *cliente = buscarClientePorCPF(cpf);
+void extrato(BancoDados *dados, const char *cpf, const char *senha) {
+    Cliente *cliente = buscarClientePorCPF(dados, cpf);
     if (cliente && strcmp(cliente->senha, senha) == 0) {
         FILE *file = fopen(EXTRATO_FILE, "w");
-        for (int i = 0; i < numTransacoes; i++) {
-            if (strcmp(transacoes[i].cpf, cpf) == 0) {
+        for (int i = 0; i < dados->numTransacoes; i++) {
+            if (strcmp(dados->transacoes[i].cpf, cpf) == 0) {
                 fprintf(file, "Tipo: %s, Valor: %.2f\n",
-                        transacoes[i].tipo == DEPOSITO ? "Depósito" : "Débito",
-                        transacoes[i].valor);
+                        dados->transacoes[i].tipo == DEPOSITO ? "Depósito" : "Débito",
+                        dados->transacoes[i].valor);
             }
         }
         fclose(file);
@@ -125,9 +116,9 @@ void extrato(const char *cpf, const char *senha) {
 }
 
 // Função para transferir dinheiro entre dois clientes
-void transferir(char cpf[], char senha[], char cpfDestino[], float valor) {
-    Cliente *clienteOrigem = buscarClientePorCPF(cpf);
-    Cliente *clienteDestino = buscarClientePorCPF(cpfDestino);
+void transferir(BancoDados *dados, char cpf[], char senha[], char cpfDestino[], float valor) {
+    Cliente *clienteOrigem = buscarClientePorCPF(dados, cpf);
+    Cliente *clienteDestino = buscarClientePorCPF(dados, cpfDestino);
 
     if (clienteOrigem && clienteDestino && strcmp(clienteOrigem->senha, senha) == 0) {
         float taxaOrigem = (clienteOrigem->tipo == COMUM) ? 0.05 : 0.03;
@@ -142,14 +133,14 @@ void transferir(char cpf[], char senha[], char cpfDestino[], float valor) {
             strcpy(tOrigem.cpf, cpf);
             tOrigem.tipo = DEBITO;
             tOrigem.valor = debito;
-            registrarTransacao(tOrigem);
+            registrarTransacao(dados, tOrigem);
 
             // Registrar transação de depósito
             Transacao tDestino;
             strcpy(tDestino.cpf, cpfDestino);
             tDestino.tipo = DEPOSITO;
             tDestino.valor = valor;
-            registrarTransacao(tDestino);
+            registrarTransacao(dados, tDestino);
         } else {
             printf("Saldo insuficiente.\n");
         }
@@ -157,75 +148,60 @@ void transferir(char cpf[], char senha[], char cpfDestino[], float valor) {
         printf("CPF ou senha incorretos ou cliente destino não encontrado.\n");
     }
 }
-
 // Função para registrar uma nova transação, seja de débito ou depósito.
-void registrarTransacao(Transacao t) {
-    // Verifica se o limite de transações foi atingido
-    if (numTransacoes < MAX_TRANSACOES) {
-        // Adiciona a transação ao array de transações
-        transacoes[numTransacoes] = t;
-        numTransacoes++;
+void registrarTransacao(BancoDados *dados, Transacao t) {
+    if (dados->numTransacoes < MAX_TRANSACOES) {
+        dados->transacoes[dados->numTransacoes] = t;
+        dados->numTransacoes++;
     } else {
-        // Exibe uma mensagem de erro se o limite de transações for atingido
         printf("Limite de transações atingido.\n");
     }
 }
 
 // Função para salvar os clientes em um arquivo binário
-void salvarClientesArquivo() {
-    // Abre o arquivo para gravação em modo binário
+void salvarClientesArquivo(BancoDados *dados) {
     FILE *file = fopen(CLIENTES_FILE, "wb");
     if (file) {
-        // Escreve a quantidade de clientes e depois os dados dos clientes
-        fwrite(&numClientes, sizeof(int), 1, file);
-        fwrite(clientes, sizeof(Cliente), numClientes, file);
+        fwrite(&dados->numClientes, sizeof(int), 1, file);
+        fwrite(dados->clientes, sizeof(Cliente), dados->numClientes, file);
         fclose(file);
     } else {
-        // Exibe uma mensagem de erro se não for possível abrir o arquivo
         printf("Erro ao salvar clientes no arquivo.\n");
     }
 }
 
 // Função para carregar os clientes de um arquivo binário
-void carregarClientesArquivo() {
+void carregarClientesArquivo(BancoDados *dados) {
     FILE *file = fopen(CLIENTES_FILE, "rb");
     if (file) {
-        // Lê a quantidade de clientes e depois os dados dos clientes
-        fread(&numClientes, sizeof(int), 1, file);
-        fread(clientes, sizeof(Cliente), numClientes, file);
+        fread(&dados->numClientes, sizeof(int), 1, file);
+        fread(dados->clientes, sizeof(Cliente), dados->numClientes, file);
         fclose(file);
     } else {
-        // Exibe uma mensagem de erro se não for possível abrir o arquivo
         printf("Erro ao carregar clientes do arquivo.\n");
     }
 }
 
 // Função para salvar as transações em um arquivo binário
-void salvarTransacoesArquivo() {
-    // Abre o arquivo para gravação em modo binário
+void salvarTransacoesArquivo(BancoDados *dados) {
     FILE *file = fopen(TRANSACOES_FILE, "wb");
     if (file) {
-        // Escreve a quantidade de transações e depois os dados das transações
-        fwrite(&numTransacoes, sizeof(int), 1, file);
-        fwrite(transacoes, sizeof(Transacao), numTransacoes, file);
+        fwrite(&dados->numTransacoes, sizeof(int), 1, file);
+        fwrite(dados->transacoes, sizeof(Transacao), dados->numTransacoes, file);
         fclose(file);
     } else {
-        // Exibe uma mensagem de erro se não for possível abrir o arquivo
         printf("Erro ao salvar transações no arquivo.\n");
     }
 }
 
 // Função para carregar as transações de um arquivo binário
-void carregarTransacoesArquivo() {
+void carregarTransacoesArquivo(BancoDados *dados) {
     FILE *file = fopen(TRANSACOES_FILE, "rb");
     if (file) {
-        // Lê a quantidade de transações e depois os dados das transações
-        fread(&numTransacoes, sizeof(int), 1, file);
-        fread(transacoes, sizeof(Transacao), numTransacoes, file);
+        fread(&dados->numTransacoes, sizeof(int), 1, file);
+        fread(dados->transacoes, sizeof(Transacao), dados->numTransacoes, file);
         fclose(file);
     } else {
-        // Exibe uma mensagem de erro se não for possível abrir o arquivo
-        // Tive alguns problemas fazendo essa parte, a leitura estava dando errado
         printf("Erro ao carregar transações do arquivo.\n");
     }
 }
